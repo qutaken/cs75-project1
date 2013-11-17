ba34b9bba2ddb216236e6fed87c104f194f7f2b8
Velvel <velvel53@gmail.com>
2013-11-17 01:05:39 -0200

    finished up the register added password validation on client and server

--- controller/register.php
+++ controller/register.php
@@ -17,6 +17,11 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["re_pass
 	{
 		render('register', array('error' => "Passwords don't match."));
 	}
+	else if (strlen($_POST['password']) < 7 ||
+			(!preg_match('/((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i', $_POST['password']))
+	{
+		render('register', array('error' => "Not a valid password."));	
+	}
 	else
 	{
 		$email = $_POST["email"]; $password = $_POST["password"];
--- view/register.php
+++ view/register.php
@@ -42,6 +42,10 @@ function validateForm()
 	re_password = $("input[name=re_password]");
 	if (emailField.val().length < 6)
 		isValid = false;
+	else if (password.length() < 7)
+		isValid = false;
+	else if (!password.match(/((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i))
+		isValid = false;
 	else if (password.localeCompare(re_password)
 		isValid = false;
 
